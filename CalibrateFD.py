1b8bdc6cbd8b46520010202503a06461209e16b6
XiuqiWang <x.wang-8@utwente.nl>
2025-11-05 17:16:22 +0100

    Mdrag with the CD form is more suitable for wet case

--- CalibrateFD.py
+++ CalibrateFD.py
@@ -30,16 +30,16 @@ def drag_model(x, b, CD):
     U_rel = ueff - U
     return np.pi*D**2/8 * rho_air * CD * np.abs(U_rel) * U_rel  
 
-def drag_model_ori(x, b):
-    Ua, U = x
-    Ueff = b*Ua
-    Urel = Ueff-U
-    Re = abs(Urel)*D/nu_a
-    Ruc = 24
-    Cd_inf = 0.5
-    Cd = (np.sqrt(Cd_inf)+np.sqrt(Ruc/Re))**2   
-    Mdrag = np.pi/8 * D**2 * rho_air * Urel * abs(Urel) * Cd
-    return Mdrag
+# def drag_model_ori(x, b):
+#     Ua, U = x
+#     Ueff = b*Ua
+#     Urel = Ueff-U
+#     Re = abs(Urel)*D/nu_a
+#     Ruc = 24
+#     Cd_inf = 0.5
+#     Cd = (np.sqrt(Cd_inf)+np.sqrt(Ruc/Re))**2   
+#     Mdrag = np.pi/8 * D**2 * rho_air * Urel * abs(Urel) * Cd
+#     return Mdrag
 
 def BinfdUa(Ua, fd, U, Uabin):
     Ua = np.asarray(Ua, dtype=float)
@@ -111,20 +111,20 @@ k = 5e-9
 C_all_S, U_all_S, Ua_all_S, MD_all_S, fd_ori, fd_ori_se, fd_com = [], [], [], [], [], [], []
 for i in range(2, 7):
     # ---- Load data ----
-    file_fd = f'TotalDragForce/FD_S00{i}dry.txt'
+    file_fd = f'TotalDragForce/Mdrag/FD_S00{i}Dry.txt'
     data_FD = np.loadtxt(file_fd)
     MD = data_FD
-    file_c = f'CGdata/hb=12d/Shields00{i}dry.txt'
+    file_c = f'CGdata/hb=13.5d/Shields00{i}Dry135d.txt'
     data_dpm = np.loadtxt(file_c)
     Q_dpm = data_dpm[:, 0]
     C_dpm = data_dpm[:, 1]
     U_dpm = data_dpm[:, 2]
-    file_ua = f'TotalDragForce/Uair_ave-tS00{i}Dryh02.txt'
+    file_ua = f'TotalDragForce/Ua-t/Uair_ave-tS00{i}Dry.txt'
     Ua_dpm = np.loadtxt(file_ua, delimiter='\t')[:, 1]
     
     # binning Ua and getting the mean of RHS
     fd_dpm = MD*mp/C_dpm
-    fd_dpm_binned, fd_dpm_se, U_dpm_binned, Ua_dpm_binned = BinfdUa(Ua_dpm, fd_dpm, U_dpm, Ua_bin)
+    # fd_dpm_binned, fd_dpm_se, U_dpm_binned, Ua_dpm_binned = BinfdUa(Ua_dpm, fd_dpm, U_dpm, Ua_bin)
     
     # Combine
     U_all_S.append(U_dpm)
@@ -136,22 +136,24 @@ U_all = np.concatenate(U_all_S)
 Ua_all = np.concatenate(Ua_all_S)
 fd_all = np.concatenate(fd_ori)
 # fd_se_all = np.concatenate(fd_ori_se)
-# mask = np.isfinite(U_all) & np.isfinite(Ua_all) & np.isfinite(fd_all) & np.isfinite(fd_se_all)
-# U_all, Ua_all, fd_all, fd_se_all = U_all[mask], Ua_all[mask], fd_all[mask], fd_se_all[mask]
+mask = np.isfinite(U_all) & np.isfinite(Ua_all) & np.isfinite(fd_all) 
+U_all, Ua_all, fd_all = U_all[mask], Ua_all[mask], fd_all[mask]
 
 p0 = (0.4, 5e-9)
-popt, _ = curve_fit(drag_model_ori, (Ua_all, U_all), fd_all, absolute_sigma=True, maxfev=20000)
-b_hat = popt[0]
-print(f'b_hat={b_hat:.2f}')
-fd_pred = drag_model_ori((Ua_all, U_all), b_hat)
+popt, _ = curve_fit(drag_model, (Ua_all, U_all), fd_all, absolute_sigma=True, maxfev=20000)
+b, CD = popt
+print(f'b={b:.2f}')
+print(f'CD={CD:.2f}')
+
+fd_pred = drag_model((Ua_all, U_all), b, CD)
 r2 = r2_score(fd_all, fd_pred)
 print('r2', r2)
 
-plt.close('all')
+# plt.close('all')
 plt.figure(figsize=(12, 10))
 for i in range(5):
     plt.subplot(3, 2, i + 1)
-    fd_com = drag_model_ori((Ua_all_S[i], U_all_S[i]), b_hat)
+    fd_com = drag_model((Ua_all_S[i], U_all_S[i]), b, CD)
     plt.plot(Ua_all_S[i], fd_ori[i], 'o', label='DPM')
     plt.plot(Ua_all_S[i], fd_com, 'o', label='fit')
     plt.title(f"S00{i+2} Dry")
